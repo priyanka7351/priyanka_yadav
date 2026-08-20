9c42ff59b43e8aacd3ee2a6e2366ab5c09505cda
priyanka7351 <priyankayadav9105@gmail.com>
2026-08-20 14:08:25 +0530

    feat: add Distribute Elements Into Two Arrays I solution

--- algorithms/distribute-elements-into-two-arrays-i/solution.py
+++ algorithms/distribute-elements-into-two-arrays-i/solution.py
@@ -1,34 +1,14 @@
 class Solution:
-    def maxNumberOfFamilies(self, n: int, reservedSeats: List[List[int]]) -> int:
+    def resultArray(self, nums: List[int]) -> List[int]:
+    
+        arr1 = [nums[0]]
+        arr2 = [nums[1]]
 
-        reserved = {}
+        for i in range(2, len(nums)):
+            if arr1[-1] > arr2[-1]:
+                arr1.append(nums[i])
+            else:
+                arr2.append(nums[i])
 
-        # Store reserved seats for each row
-        for row, seat in reservedSeats:
-            if row not in reserved:
-                reserved[row] = set()
-
-            reserved[row].add(seat)
-
-        # Every row initially contributes 2 groups
-        result = 2 * n
-
-        # Recalculate only rows containing reservations
-        for row in reserved:
-            result -= 2
-
-            seats = reserved[row]
-
-            left_available = not any(seat in seats for seat in [2, 3, 4, 5])
-
-            middle_available = not any(seat in seats for seat in [4, 5, 6, 7])
-
-            right_available = not any(seat in seats for seat in [6, 7, 8, 9])
-
-            if left_available and right_available:
-                result += 2
-            elif left_available or middle_available or right_available:
-                result += 1
-
-        return result
+        return arr1 + arr2
         
