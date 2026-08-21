1bbe76e1fa455a418c860933f53924daa711d363
priyanka7351 <priyankayadav9105@gmail.com>
2026-08-22 03:16:38 +0530

    feat: add Kth Smallest Amount With Single Denomination Combination solution

--- algorithms/kth-smallest-amount-with-single-denomination-combination/solution.py
+++ algorithms/kth-smallest-amount-with-single-denomination-combination/solution.py
@@ -1,14 +1,55 @@
+from math import gcd
 class Solution:
-    def resultArray(self, nums: List[int]) -> List[int]:
-    
-        arr1 = [nums[0]]
-        arr2 = [nums[1]]
+    def findKthSmallest(self, coins: List[int], k: int) -> int:
+        
 
-        for i in range(2, len(nums)):
-            if arr1[-1] > arr2[-1]:
-                arr1.append(nums[i])
+        def lcm(a, b):
+            return a // gcd(a, b) * b
+
+        n = len(coins)
+
+        # Count how many distinct amounts <= x can be made
+        def count(x):
+            total = 0
+
+            # Inclusion-Exclusion
+            for mask in range(1, 1 << n):
+                curr_lcm = 1
+                bits = 0
+                valid = True
+
+                for i in range(n):
+                    if mask & (1 << i):
+                        bits += 1
+                        curr_lcm = lcm(curr_lcm, coins[i])
+
+                        if curr_lcm > x:
+                            valid = False
+                            break
+
+                if not valid:
+                    continue
+
+                values = x // curr_lcm
+
+                if bits % 2 == 1:
+                    total += values
+                else:
+                    total -= values
+
+            return total
+
+        # Binary Search
+        left = 1
+        right = min(coins) * k
+
+        while left < right:
+            mid = (left + right) // 2
+
+            if count(mid) >= k:
+                right = mid
             else:
-                arr2.append(nums[i])
+                left = mid + 1
 
-        return arr1 + arr2
+        return left
         
