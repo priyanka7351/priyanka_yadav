d970747d6faafbf47497ce5751cdfa0abc0fb993
priyanka7351 <priyankayadav9105@gmail.com>
2026-09-25 20:36:36 +0530

    feat: add Brace Expansion II solution

--- algorithms/brace-expansion-ii/solution.java
+++ algorithms/brace-expansion-ii/solution.java
@@ -1,25 +1,74 @@
+import java.util.*;
+
 class Solution {
-    public int smallestIndex(int[] nums) {
-        
 
-        for (int i = 0; i < nums.length; i++) {
+    int index = 0;
 
-            int n = nums[i];
-            int sum = 0;
+    public List<String> braceExpansionII(String expression) {
 
-            while (n > 0) {
-                int digit = n % 10;
-                sum += digit;
-                n = n / 10;
+        Set<String> result = parse(expression);
+
+        List<String> ans = new ArrayList<>(result);
+        Collections.sort(ans);
+
+        return ans;
+    }
+
+    private Set<String> parse(String s) {
+
+        Set<String> result = new HashSet<>();
+        Set<String> current = new HashSet<>();
+
+        current.add("");
+
+        while (index < s.length() && s.charAt(index) != '}') {
+
+            char ch = s.charAt(index);
+
+            Set<String> part;
+
+            if (ch == '{') {
+
+                index++; // skip {
+
+                part = parse(s);
+
+                index++; // skip }
+
+            } 
+            else {
+
+                part = new HashSet<>();
+                part.add(String.valueOf(ch));
+
+                index++;
             }
 
-            if (sum == i) {
-                return i;
+            // Concatenation
+            Set<String> next = new HashSet<>();
+
+            for (String a : current) {
+                for (String b : part) {
+                    next.add(a + b);
+                }
+            }
+
+            current = next;
+
+            // Union
+            if (index < s.length() && s.charAt(index) == ',') {
+
+                result.addAll(current);
+
+                current.clear();
+                current.add("");
+
+                index++;
             }
         }
 
-        return -1;
-    
-        
+        result.addAll(current);
+
+        return result;
     }
 }
